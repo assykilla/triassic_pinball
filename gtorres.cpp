dad704469c4c395aa7a9c8088a820f11abe68e10
unknown <gtorres64>
2023-03-14 09:35:40 -0700

    more testing

--- gtorres.cpp
+++ gtorres.cpp
@@ -1,19 +1,2 @@
 //George Torres
 
-void georgeFeature()
-{
-    ggprint8b(&r[6], 20, 0x0034a4eb, "George's Feature");
-
-    if (XK_leftarrow) {
-        ball.wid -= .005;
-    }
-    if (XK_righarrow) {
-        ball.wid += .005;
-    }
-    if (XK_uparrow) {
-        ball.hgt += .005;
-    }
-    if (XK_uparrow) {
-        ball.hgt -= .005;
-    }
-}
